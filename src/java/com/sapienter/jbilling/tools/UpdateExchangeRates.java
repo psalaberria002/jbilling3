413cde1296e3d31d24bb6927dbd086e984dc672f
psalaberria002 <psalaberria002@gmail.com>
2013-03-18 11:59:32 +0100

    Precision of the BigDecimal set up to 4.
    Added logo 10.

--- src/java/com/sapienter/jbilling/tools/UpdateExchangeRates.java
+++ src/java/com/sapienter/jbilling/tools/UpdateExchangeRates.java
@@ -40,7 +40,7 @@ public class UpdateExchangeRates {
 	public static void updateMap(Document doc, Map<String, BigDecimal> map) {
 		// do something with the current node instead of System.out
 		NodeList nodes = doc.getElementsByTagName("Cube");
-		BigDecimal eurRate = new BigDecimal(0.0000).setScale(2, BigDecimal.ROUND_HALF_UP);
+		BigDecimal eurRate = new BigDecimal(0);
 		for (int i = 0; i < nodes.getLength(); i++) {
 			Node node = nodes.item(i);
 			System.out.println(i);
@@ -52,7 +52,7 @@ public class UpdateExchangeRates {
 					String attribute2 = child.getAttribute("rate");
 					System.out.println(attribute + attribute2);
 					if (attribute.equals("USD")) {
-						eurRate = (new BigDecimal(1.0000).setScale(4, BigDecimal.ROUND_HALF_UP)).divide(new BigDecimal(
+						eurRate = (new BigDecimal(1)).divide(new BigDecimal(
 								attribute2),4, BigDecimal.ROUND_HALF_UP);
 						map.put("EUR", eurRate);
 					} else if (attribute != null) {
